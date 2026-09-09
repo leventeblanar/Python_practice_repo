a9597e96fb4d9b35321ef1c2cc8ab5ea881c002a
Levente Blanar <blanar.levente@gmail.com>
2026-09-09 14:21:15 +0200

    exercism

--- practice_modules/os/basics.py
+++ practice_modules/os/basics.py
@@ -7,6 +7,19 @@ ENV_PATH = PROJECT_ROOT / ".env"
 
 load_dotenv(ENV_PATH)
 
-env = os.getenv("ORDERS_PATH")
+orders_path = os.getenv("ORDERS_PATH")
+app_env = os.getenv("APP_ENV")
+timeout = os.getenv("TIMEOUT", "30")
 
-print(env)
+if not orders_path:
+    print(f"ORDERS_PATH is missing")
+
+
+print(f"Orders path: {orders_path}")
+print(f"Environment: {app_env}")
+print(f"Timeout: {timeout}")
+
+# Ha az összes env változót akarom berakni egybe:
+env_variables = os.environ
+
+print(f"orders_path: {env_variables['ORDERS_PATH']}")
