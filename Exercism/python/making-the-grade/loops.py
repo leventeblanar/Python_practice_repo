03a7adeed27478c88b31fe51ed6eb780b3dbecdf
Levente Blanar <blanar.levente@gmail.com>
2026-09-22 15:25:42 +0200

    making-the-grade

---
+++ Exercism/python/making-the-grade/loops.py
@@ -0,0 +1,92 @@
+"""Functions for organizing and calculating student exam scores."""
+
+
+def round_scores(student_scores: list) -> list:
+    """Round all provided student scores.
+
+    Parameters:
+        student_scores (list[float]): Student exam scores.
+
+    Returns:
+        list[int]: Student scores *rounded* to the nearest integer value.
+    """
+
+    return [round(student_score) for student_score in student_scores]
+
+
+def count_failed_students(student_scores: list) -> int:
+    """Count the number of failing students out of the group provided.
+
+    Parameters:
+        student_scores (list[int]): Student scores as ints.
+
+    Returns:
+        int: The count of student scores at or below 40.
+    """
+
+    return len([score for score in student_scores if score <= 40])
+
+
+def above_threshold(student_scores: list[int], threshold: int) -> list[int]:
+    """Determine how many of the provided student scores were 'the best' based on the provided threshold.
+
+    Parameters:
+        student_scores (list[int]): Integer scores.
+        threshold (int): The threshold to cross to be the "best" score.
+
+    Returns:
+        list[int]: Integer scores that are at or above the "best" threshold.
+    """
+
+    return [score for score in student_scores if score >= threshold]
+
+
+def letter_grades(highest: int):
+    """Create a list of grade thresholds based on the provided highest grade.
+
+    Parameters:
+        highest (int): The value of the highest exam score.
+
+    Returns:
+        list[int]: Lower threshold scores for each D-A letter grade interval.
+
+        For example, where the highest score is 100, and failing is <= 40,
+        The result would be [41, 56, 71, 86]:
+            41 <= "D" <= 55
+            56 <= "C" <= 70
+            71 <= "B" <= 85
+            86 <= "A" <= 100
+    """
+
+    step = (highest - 40) // 4
+    return [41 + step * grade_range for grade_range in range(4)]
+
+
+def student_ranking(student_scores: list, student_names: list[str]) -> list[str]:
+    """Organize the student's rank, name, and grade information in descending order.
+
+    Parameters:
+        student_scores (list): Scores in descending order.
+        student_names (list[str]): Student names by exam score in descending order.
+
+    Returns:
+        list[str]: Strings in format ["<rank>. <student name>: <score>"].
+    """
+
+    return [f"{rank}. {name}: {score}" for rank, (name, score) in enumerate(zip(student_names, student_scores), start=1)]
+
+
+def perfect_score(student_info):
+    """Create a list that contains the name and grade of the first student to make a perfect score on the exam.
+
+    Parameters:
+        student_info (list[list[str, int]]): List of [<student name>, <score>] lists.
+
+    Returns:
+        list: First `[<student name>, 100]` found OR `[]` if no student score of 100 is found.
+    """
+
+    for name, score in student_info:
+        if score == 100:
+            return[name, score]
+    return []
